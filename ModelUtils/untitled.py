a836bb1c26faba57354a325fb985e871104d2e23
Thomaskutty Reji <thomasreji.kutty@gmail.com>
2023-01-27 09:59:14 +0530

    updated

---
+++ ModelUtils/untitled.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+class ModelPerformance:
+    pass 
+
+class Model:
+    def __init__(self,x_train,x_test,y_trian,y_test):
+    pass 
+
+class Analysis:
+    def __init__(self,x,y,x_train,x_test,y_train,y_test):
+    pass 
+
